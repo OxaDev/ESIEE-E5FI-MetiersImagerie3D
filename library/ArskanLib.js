1ea16248c094c83d3f7d20c47200aaa8e0bd41ac
unknown <daphne.bissonnier@edu.esiee.fr>
2022-04-01 18:46:31 +0200

    front fonctionnel

--- library/ArskanLib.js
+++ library/ArskanLib.js
@@ -1,6 +1,6 @@
-class ArskanLib{
+class ArskanLib {
 
-    constructor(){
+    constructor() {
         this._this = this;
         this.apiToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNlbGluZUBrcmFiYmkuZnIiLCJzaWxvIjoiNjIxNjVmOWFlZjAwZjAyZTEzYzc2ZDdjIiwiaWF0IjoxNjQ2OTA5MzExLCJpc3MiOiJodHRwczovL2FwaS5hcnNrYW4uY29tIn0.ENwjv5bwwtwxOWcGMxJ4CZ-GrhamEfDOMjPmm2Bpdac";
         this.profileToken = "5fd0a6c48568fc630e5be379"
@@ -10,75 +10,75 @@ class ArskanLib{
         Objects
     */
 
-    getAllObjectFromSilo(){
+    getAllObjectFromSilo() {
         var myHeaders = new Headers();
         myHeaders.append("Accept", "application/json");
         myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer "+ this.apiToken);
+        myHeaders.append("Authorization", "Bearer " + this.apiToken);
 
         var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
         };
 
         return fetch("https://public-api.arskan.com/objects", requestOptions)
-        .then(response => response.text())
-        .then(result => {return JSON.parse(result);})
-        .catch(error => {return JSON.parse(error); });
+            .then(response => response.text())
+            .then(result => { return JSON.parse(result); })
+            .catch(error => { return JSON.parse(error); });
 
-        
+
     }
 
-    getEmbedUrlOfObjectFromSilo(objectIDString){
+    getEmbedUrlOfObjectFromSilo(objectIDString) {
         var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer "+this.apiToken);
+        myHeaders.append("Authorization", "Bearer " + this.apiToken);
         myHeaders.append("Content-Type", "application/json");
 
         var raw = JSON.stringify({
-        "profile": this.profileToken,
-        "name": "lien"
+            "profile": this.profileToken,
+            "name": "lien"
         });
 
         var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
         };
 
-        return fetch("https://public-api.arskan.com/objects/"+objectIDString+"/embed", requestOptions)
-        .then(response => response.text())
-        .then(result => {return JSON.parse(result);})
-        .catch(error => {return JSON.parse(error); });
+        return fetch("https://public-api.arskan.com/objects/" + objectIDString + "/embed", requestOptions)
+            .then(response => response.text())
+            .then(result => { return JSON.parse(result); })
+            .catch(error => { return JSON.parse(error); });
     }
 
-    getOneObjectFromSilo(objectIDString){
+    getOneObjectFromSilo(objectIDString) {
         var myHeaders = new Headers();
         myHeaders.append("Accept", "application/json");
         myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer "+ this.apiToken);
+        myHeaders.append("Authorization", "Bearer " + this.apiToken);
 
         var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
         };
 
-        return fetch("https://public-api.arskan.com/objects/"+objectIDString, requestOptions)
-        .then(response => response.text())
-        .then(result => {return JSON.parse(result);} )
-        .catch(error => {return JSON.parse(error); });
+        return fetch("https://public-api.arskan.com/objects/" + objectIDString, requestOptions)
+            .then(response => response.text())
+            .then(result => { return JSON.parse(result); })
+            .catch(error => { return JSON.parse(error); });
 
     }
-    
+
     /*
         Pointers
     */
 
-    addPointerToObject(objectIDString, pointerDataJSON){
+    addPointerToObject(objectIDString, pointerDataJSON) {
         var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer "+ this._this.apiToken);
+        myHeaders.append("Authorization", "Bearer " + this._this.apiToken);
         myHeaders.append("Content-Type", "application/json");
 
         /*var raw = JSON.stringify(
@@ -106,74 +106,74 @@ class ArskanLib{
         var raw = JSON.stringify(pointerDataJSON);
 
         var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
         };
 
-        fetch("https://public-api.arskan.com/objects/"+objectIDString+"/pointers", requestOptions)
-        .then(response => response.text())
-        .then(result => {return JSON.parse(result);})
-        .catch(error => {return JSON.parse(error); });
+        return fetch("https://public-api.arskan.com/objects/" + objectIDString + "/pointers", requestOptions)
+            .then(response => response.text())
+            .then(result => { return JSON.parse(result); })
+            .catch(error => { return JSON.parse(error); });
     }
 
-    deletePointerFromObject(pointerIDString){
+    deletePointerFromObject(pointerIDString) {
         var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer "+this.apiToken);
+        myHeaders.append("Authorization", "Bearer " + this.apiToken);
 
         var requestOptions = {
-        method: 'DELETE',
-        headers: myHeaders,
-        redirect: 'follow'
+            method: 'DELETE',
+            headers: myHeaders,
+            redirect: 'follow'
         };
 
-        fetch("https://public-api.arskan.com/pointers/"+pointerIDString, requestOptions)
-        .then(response => response.text())
-        .then(result => {return JSON.parse(result);})
-        .catch(error => {return JSON.parse(error); });
-            }
+        return fetch("https://public-api.arskan.com/pointers/" + pointerIDString, requestOptions)
+            .then(response => response.text())
+            .then(result => { return result; })
+            .catch(error => { return JSON.parse(error); });
+    }
 
-    getAllPointersFromObject(objectIDString){
+    getAllPointersFromObject(objectIDString) {
         var myHeaders = new Headers();
         myHeaders.append("Accept", "application/json");
         myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer "+ this.apiToken);
+        myHeaders.append("Authorization", "Bearer " + this.apiToken);
 
-        var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-        };
-
-        return fetch("https://public-api.arskan.com/objects/"+objectIDString+"/pointers", requestOptions)
-        .then(response => response.text())
-        .then(result => {return JSON.parse(result);})
-        .catch(error => {return JSON.parse(error); });
-
-
-    }
-
-    getOnePointerFromSilo(pointerIDString){
-        var myHeaders = new Headers();
-        myHeaders.append("Accept", "application/json");
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer "+ this.apiToken);
         var requestOptions = {
             method: 'GET',
             headers: myHeaders,
             redirect: 'follow'
         };
-        
-        return fetch("https://public-api.arskan.com/pointers/"+pointerIDString, requestOptions)
-        .then(response => response.text())
-        .then(result => {return JSON.parse(result);})
-        .catch(error => {return JSON.parse(error); });
+
+        return fetch("https://public-api.arskan.com/objects/" + objectIDString + "/pointers", requestOptions)
+            .then(response => response.text())
+            .then(result => { return JSON.parse(result); })
+            .catch(error => { return JSON.parse(error); });
+
+
     }
 
-    updatePointerFromObject(pointerIDString, pointerDataJSON){
+    getOnePointerFromSilo(pointerIDString) {
         var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer "+this.apiToken);
+        myHeaders.append("Accept", "application/json");
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", "Bearer " + this.apiToken);
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        return fetch("https://public-api.arskan.com/pointers/" + pointerIDString, requestOptions)
+            .then(response => response.text())
+            .then(result => { return JSON.parse(result); })
+            .catch(error => { return JSON.parse(error); });
+    }
+
+    updatePointerFromObject(pointerIDString, pointerDataJSON) {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + this.apiToken);
         myHeaders.append("Content-Type", "application/json");
 
         /*var raw = JSON.stringify({
@@ -201,37 +201,37 @@ class ArskanLib{
         var raw = JSON.stringify(pointerDataJSON);
 
         var requestOptions = {
-        method: 'PUT',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
+            method: 'PUT',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
         };
 
-        fetch("https://public-api.arskan.com/pointers/"+pointerIDString, requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+        return fetch("https://public-api.arskan.com/pointers/" + pointerIDString, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
     }
 
     /*
         Silos
     */
-    getCurrentSilo(){
+    getCurrentSilo() {
         var myHeaders = new Headers();
         myHeaders.append("Accept", "application/json");
         myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer "+ this.apiToken);
+        myHeaders.append("Authorization", "Bearer " + this.apiToken);
 
         var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
         };
 
         return fetch("https://public-api.arskan.com/silos/current", requestOptions)
-        .then(response => response.text())
-        .then(result => {return JSON.parse(result);})
-        .catch(error => {return JSON.parse(error); });
+            .then(response => response.text())
+            .then(result => { return JSON.parse(result); })
+            .catch(error => { return JSON.parse(error); });
 
     }
 
@@ -239,22 +239,22 @@ class ArskanLib{
         Profiles
     */
 
-    getProfilesViewer(){
+    getProfilesViewer() {
         var myHeaders = new Headers();
         myHeaders.append("Accept", "application/json");
         myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer "+ this.apiToken);
+        myHeaders.append("Authorization", "Bearer " + this.apiToken);
 
         var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
         };
 
         return fetch("https://public-api.arskan.com/profiles")
-        .then(response => response.text())
-        .then(result => {return JSON.parse(result);})
-        .catch(error => {return JSON.parse(error); });
+            .then(response => response.text())
+            .then(result => { return JSON.parse(result); })
+            .catch(error => { return JSON.parse(error); });
 
     }
 
